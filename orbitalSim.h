deedbe768d9b9fdcc624e19dc699e2d85a725642
lforchiassin <lforchiassin@itba.edu.ar>
2025-08-12 15:13:42 -0300

    Add files via upload

---
+++ orbitalSim.h
@@ -0,0 +1,45 @@
+﻿/**
+ * @brief Orbital simulation
+ * @author Marc S. Ressl
+ *
+ * @copyright Copyright (c) 2022-2023
+ */
+
+#ifndef ORBITALSIM_H
+#define ORBITALSIM_H
+#include "raylib.h"
+
+/**
+ * @brief Orbital body definition
+ */
+struct OrbitalBody
+{
+    // Fill in your code here...
+    Vector3 position;
+    Vector3 velocity;
+    float mass;
+	float radius;
+	Color color;
+};
+
+
+/**
+ * @brief Orbital simulation definition
+ */
+struct OrbitalSim
+{
+    // Fill in your code here...
+   float timeStep; // Time step in seconds
+   OrbitalBody *bodies; // Array of orbital bodies
+   int bodyCount; // Number of orbital bodies
+   float centerMass; // Mass of the most massive object in the star system
+   float centerRadius; // Radius of the most massive object in the star system
+
+};
+
+OrbitalSim *constructOrbitalSim(float timeStep);
+void destroyOrbitalSim(OrbitalSim *sim);
+
+void updateOrbitalSim(OrbitalSim *sim);
+
+#endif
